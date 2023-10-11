21427f6a2b2cc600859210a8de8fc57f4c1f41f0
Pedro Andrés <58451141+pvc0720@users.noreply.github.com>
2023-10-11 17:18:55 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,3 +1,226 @@
+<<<<<<< HEAD
+#include <iostream>
+#include <fstream>
+#include <bitset>
+#include <string>
+
+using namespace std;
+
+string encodeMethod1(const string& text, int n, const string& inputFileName, const string& outputFileName) {
+        // Abre los archivos de entrada y salida usando inputFileName y outputFileName
+        std::ifstream inputFile(inputFileName);
+        std::ofstream outputFile(outputFileName);
+
+    char character;
+    while (inputFile.get(character)) {
+        // Convierte el carácter en su representación binaria
+        std::bitset<8> binary(character);
+        std::bitset<8> encodedBinary; // Crear un bitset para almacenar el resultado codificado
+
+        // Mostrar la conversión inicial en binario en la consola
+        std::cout << "Carácter '" << character << "' en binario: " << binary << std::endl;
+
+            // Realiza la codificación para cada bloque de n bits
+            for (int i = 0; i < 8; i += n) {
+            std::bitset<8> block;
+            for (int j = 0; j < n; j++) {
+                if (i + j < 8) {
+                    block[j] = binary[i + j];
+                }
+            }
+
+            int countOnes = block.count(); // Contar unos en el bloque
+            int countZeros = n - countOnes; // Calcular el número de ceros
+
+            for (int j = 0; j < n; j++) {
+                if (countOnes == countZeros) {
+                    encodedBinary[i + j] = !block[j]; // Invertir cada bit si hay igual cantidad de unos y ceros
+                } else if (countZeros > countOnes) {
+                    if (j % 2 == 0) {
+                        encodedBinary[i + j] = !block[j]; // Invertir cada 2 bits si hay más ceros
+                    } else {
+                        encodedBinary[i + j] = block[j];
+                    }
+                } else {
+                    if (j % 3 == 0) {
+                        encodedBinary[i + j] = !block[j]; // Invertir cada 3 bits si hay más unos
+                    } else {
+                        encodedBinary[i + j] = block[j];
+                    }
+                }
+            }
+        }
+
+        // Escribe la representación binaria codificada en el archivo de salida
+        outputFile << encodedBinary;
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    std::cout << "Cadena de caracteres codificada y guardada en output.txt." << std::endl;
+
+    return 0;
+}
+
+string decodeMethod1(const string& encodedText, int n) {
+    std::ifstream inputFile("output.txt");
+    std::ofstream outputFile("decoded_output.txt");
+
+    std::bitset<8> encodedBinary;
+    while (inputFile >> encodedBinary) {
+        // Realiza la decodificación para cada bloque de n bits
+        std::bitset<8> decodedBinary;
+
+        for (int i = 0; i < 8; i += n) {
+            std::bitset<8> block;
+            for (int j = 0; j < n; j++) {
+                if (i + j < 8) {
+                    block[j] = encodedBinary[i + j];
+                }
+            }
+
+            int countOnes = block.count(); // Contar unos en el bloque
+            int countZeros = n - countOnes; // Calcular el número de ceros
+
+            for (int j = 0; j < n; j++) {
+                if (countOnes == countZeros) {
+                    decodedBinary[i + j] = !block[j]; // Invertir cada bit si hay igual cantidad de unos y ceros
+                } else if (countZeros > countOnes) {
+                    if (j % 2 == 0) {
+                        decodedBinary[i + j] = !block[j]; // Invertir cada 2 bits si hay más ceros
+                    } else {
+                        decodedBinary[i + j] = block[j];
+                    }
+                } else {
+                    if (j % 3 == 0) {
+                        decodedBinary[i + j] = !block[j]; // Invertir cada 3 bits si hay más unos
+                    } else {
+                        decodedBinary[i + j] = block[j];
+                    }
+                }
+            }
+        }
+
+        // Convierte el bitset decodificado en un carácter y escribe en el archivo de salida
+        char decodedCharacter = static_cast<char>(decodedBinary.to_ulong());
+        outputFile << decodedCharacter;
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    std::cout << "Cadena de bits decodificada y guardada en decoded_output.txt." << std::endl;
+
+    return 0;
+}
+
+string encodeMethod2(const string& text, int n) {
+    string encodedText;
+    string binaryText;
+
+    // Convierte el texto en una cadena de 0s y 1s
+    for (char character : text) {
+        bitset<8> binary(character);
+        binaryText += binary.to_string();
+    }
+
+    // Reorganiza los bits según las especificaciones
+    for (int i = 0; i < binaryText.length(); i++) {
+        int newIndex = (i + n) % binaryText.length();
+        encodedText += binaryText[newIndex];
+    }
+
+    return encodedText;
+}
+
+string decodeMethod2(const string& encodedText, int n) {
+    string binaryText;
+    string decodedText;
+
+    // Invertir el reordenamiento de los bits
+    for (int i = 0; i < encodedText.length(); i++) {
+        int originalIndex = (i - n + encodedText.length()) % encodedText.length();
+        binaryText += encodedText[originalIndex];
+    }
+
+    // Dividir la cadena binaria en bloques de 8 bits y convertirlos a caracteres ASCII
+    for (size_t i = 0; i < binaryText.length(); i += 8) {
+        string binaryByte = binaryText.substr(i, 8);
+        bitset<8> bits(binaryByte);
+        char character = char(bits.to_ulong());
+        decodedText += character;
+    }
+
+    return decodedText;
+}
+
+int main() {
+    int choice;
+    cout << "Elije una opción:\n";
+    cout << "1. Codificar (Metodo 1).\n";
+    cout << "2. Decodificar (Metodo 1).\n";
+    cout << "3. Codificar (Metodo 2).\n";
+    cout << "4. Decodificar (Metodo 2).\n";
+    cin >> choice;
+
+    if (choice < 1 || choice > 4) {
+        cerr << "Opcion no valida. Debes elegir entre 1 y 4." << endl;
+        return 1;
+    }
+
+    string inputFileName; // Nombre del archivo de entrada
+    string outputFileName; // Nombre del archivo de salida
+    int n;
+
+    cout << "Ingresa el nombre del archivo de entrada: ";
+    cin >> inputFileName;
+    cout << "Ingresa el nombre del archivo de salida (archivo .txt): ";
+    cin >> outputFileName;
+
+    ifstream inputFile(inputFileName);
+    ofstream outputFile(outputFileName);
+
+    if (!inputFile.is_open() || !outputFile.is_open()) {
+        cerr << "Error al abrir los archivos." << endl;
+        return 1;
+    }
+
+    cout << "Ingresa el valor de n: ";
+    cin >> n;
+
+    string text((istreambuf_iterator<char>(inputFile)), istreambuf_iterator<char>());
+    string result;
+
+    if (choice == 1) {
+        result = encodeMethod1(text, n, inputFileName, outputFileName);
+    } else if (choice == 2) {
+        result = decodeMethod1(text, n);
+    } else if (choice == 3) {
+        result = encodeMethod2(text, n);
+    } else {
+        result = decodeMethod2(text, n);
+    }
+
+    outputFile << result;
+
+    inputFile.close();
+    outputFile.close();
+
+    if (choice == 1) {
+        cout << "Texto codificado y guardado en " << outputFileName << "." << endl;
+    } else if (choice == 2) {
+        cout << "Texto decodificado y guardado en " << outputFileName << "." << endl;
+    } else if (choice == 3) {
+        cout << "Texto codificado y guardado en " << outputFileName << "." << endl;
+    } else {
+        cout << "Texto decodificado y guardado en " << outputFileName << "." << endl;
+    }
+
+    return 0;
+}
+
+=======
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -237,3 +460,4 @@ int main() {
 
     return 0;
 }
+>>>>>>> ec9e017d5108d382c1b67de0bc9d726bef5f8e94
